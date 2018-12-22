70fccbb321aad30e149c1f8e9cc1c145c64ee5d4
iabhishek143 <43550368+iabhishek143@users.noreply.github.com>
2018-12-22 16:38:31 +0530

    Update LargestSmallestNumber.java

--- LargestSmallestNumber.java
+++ LargestSmallestNumber.java
@@ -1,41 +1,31 @@
-class LargestSmallestNumber{
-	static int smallest(int num) {         
-        int[] freq = new int[10]; 
-        while (num > 0) { 
-            int d = num % 10; 
-            freq[d]++; 
-            num = num / 10; 
+import java.util.Arrays;
+public class MaxDigit{
+    public static void main(String[] aa){
+        int num = 760039,i=0,count=0;
+        String res="";
+        int[] arr = new int[6];
+        while(num !=0){
+            arr[i] = num%10;
+            num = num/10;
+            i++;
         }
-        int result = 0; 
-        for (int i = 1 ; i <= 9 ; i++) { 
-            if (freq[i] != 0){ 
-                result = i; 
-                freq[i]--; 
-                break; 
-            } 
-        } 
-        for (int i = 0 ; i <= 9 ; i++) 
-            while (freq[i]-- != 0) 
-                result = result * 10 + i; 
-        return result; 
-    } 
-	static int largest(int num) { 
-        int count[] = new int[10]; 
-        String str = Integer.toString(num); 
-        for(int i=0; i < str.length(); i++) 
-            count[str.charAt(i)-'0']++;  
-        int result = 0, multiplier = 1; 
-        for (int i = 0; i <= 9; i++) { 
-            while (count[i] > 0) { 
-                result = result + (i * multiplier); 
-                count[i]--; 
-                multiplier = multiplier * 10; 
-            } 
-        } 
-        return result; 
-    } 
-	public static void main(String args[]){
-		int num = 6801028; 
-        System.out.println("Given digits are:"+num+"\n Smallest: "+smallest(num)+"\n Largest: "+largest(num));
-	}
+        Arrays.sort(arr);
+        for(i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                count++;
+            }else{
+                res +=arr[i];   
+                while(count>0){
+                    res +="0";
+                    count--;
+                }
+            }
+        }
+        System.out.print("Shortest number is "+res);
+        res="";
+        for(i=arr.length-1;i>=0;i--){
+            res += arr[i];
+        }
+        System.out.print("\nLargest number is "+res);
+    }
 }
