b2eea903fa153ac6d0ac1dd906b6c924a11d0274
Nirupama Prasad <66474269+Nirupama654@users.noreply.github.com>
2020-10-01 23:16:47 +0530

    PrintingOddNumbersInAGivenRange
    This code is li'l more user-friendly as it prints the odd numbers in a range given by user

--- OddNumbers.java
+++ OddNumbers.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 class OddNumbers{
 	public static void main(String[] args){
-		int i= 20, j=40;
-		for(int k=i; k<=40; k++){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Range in which you want to find the list of Odd Numbers ?(Lower & Upper limit of the Range respectively)");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		for(int k=a; k<=b; k++){
 			if(k%2==0)
 				System.out.print(" ");
 			else
-				System.out.print(k);
+				System.out.print(" "+k+ " ");
 		}
 	}
 }
